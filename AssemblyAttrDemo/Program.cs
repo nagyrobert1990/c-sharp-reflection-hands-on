3398d131b84149b480b847eb509022eb89c938b1
nagyrobert1990 <nagy.robert1990@gmail.com>
2018-08-09 10:37:43 +0200

    all excersises are ready

---
+++ AssemblyAttrDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyAttrDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly a = Assembly.GetExecutingAssembly();
+            Type attrType = typeof(AssemblyDescriptionAttribute);
+            object[] attrs = a.GetCustomAttributes(attrType, false);
+
+            if (attrs.Length > 0)
+            {
+                AssemblyDescriptionAttribute desc = (AssemblyDescriptionAttribute)attrs[0];
+                Console.WriteLine($"Description is: {desc.Description}");
+            }
+        }
+    }
+}
--- AssemblyDemo/Program.cs
+++ AssemblyDemo/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace AssemblyDemo
 {
@@ -10,6 +7,33 @@ namespace AssemblyDemo
     {
         static void Main(string[] args)
         {
+            string path = @"C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\System.dll";
+
+            // Load a specific Assembly
+            Assembly a = Assembly.LoadFile(path);
+            ShowAssemblyInfo(a);
+
+            // Get our Assembly
+            Assembly ourAssembly = Assembly.GetExecutingAssembly();
+            ShowAssemblyInfo(ourAssembly);
+
+            Console.Read();
+        }
+
+        static void ShowAssemblyInfo(Assembly a)
+        {
+            Console.WriteLine(a.FullName);
+            Console.WriteLine("From GAC? {0}", a.GlobalAssemblyCache);
+            Console.WriteLine("Path: {0}", a.Location);
+            Console.WriteLine("Version: {0}", a.ImageRuntimeVersion);
+
+            // Show Modules
+            foreach (Module m in a.GetModules())
+            {
+                Console.WriteLine(" Mod: {0}", m.Name);
+            }
+
+            Console.WriteLine();
         }
     }
 }
